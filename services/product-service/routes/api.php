41a9a5495d0ac24d250e9159cee6153d23535d9d
AYoussef00 <abdelrahman.yousef@hadaf-hq.com>
2025-12-25 17:12:05 +0300

    Add Product Service: MySQL, Products table, Seed data, API and Health Check

---
+++ services/product-service/routes/api.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Product;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+Route::get('/health', function () {
+    return response()->json(['status' => 'ok']);
+});
+
+Route::get('/products', function () {
+    return Product::all();
+});
+
